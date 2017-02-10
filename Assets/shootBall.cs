7c57eb94ab8f8945362ca49c533dd6c48ee8f147
theshadoweffect <mcguiganriley@gmail.com>
2017-02-09 21:31:11 -0800

    Homework 4

---
+++ Assets/shootBall.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shootBall : MonoBehaviour {
+
+	public GameObject projectile;
+	public float speed = 200;
+	private GameObject clone;
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+		{
+			clone = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+			clone.GetComponent<Rigidbody>().AddForce(clone.transform.forward * speed);
+		}
+	}
+}
